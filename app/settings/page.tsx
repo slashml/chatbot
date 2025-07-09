e8c2d94080e92127b86df9f58572b8a32a0f3d3c
Tahir Khan <tahir.eslam@gmail.com>
2025-07-09 23:44:50 +0500

    bugs fixed

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -73,8 +73,6 @@ export default function SettingsPage() {
     }
   }, [session])
 //hello world
-
-
   // Save settings to localStorage on change
   const saveSettings = async () => {
     if (!session?.user?.email) return
