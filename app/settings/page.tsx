e0b652b4a926b6394b160ba98b7d98406059a707
Tahir Khan <tahir.eslam@gmail.com>
2025-07-10 00:16:35 +0500

    font size increased

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -73,6 +73,7 @@ export default function SettingsPage() {
     }
   }, [session])
 //hello world
+
   // Save settings to localStorage on change
   const saveSettings = async () => {
     if (!session?.user?.email) return
