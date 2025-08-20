828446e3aa587a09565eaccbc8f993b7a3b33772
Tahir Khan <tahir.eslam@gmail.com>
2025-08-21 04:00:02 +0500

    support page added

---
+++ app/support/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SupportPage = () => {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>Welcome to the support page. Please contact us for any assistance.</p>
+    </div>
+  );
+};
+
+export default SupportPage;
